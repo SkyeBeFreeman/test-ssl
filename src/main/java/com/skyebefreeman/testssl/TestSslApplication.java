f90c1161ebca1a9d742827d6f9e705a589415418
SkyeBeFreeman <928016560@qq.com>
2018-04-16 12:24:33 +0800

    Initial commit

---
+++ src/main/java/com/skyebefreeman/testssl/TestSslApplication.java
@@ -0,0 +1,20 @@
+package com.skyebefreeman.testssl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class TestSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestSslApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+}
